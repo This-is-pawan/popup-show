3a8dd84db12c3d0ba1cce0d1a460311ac9f50232
pawan <pawanjalandhara@gmail.com>
2024-11-16 17:20:24 +0530

    tour

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import { useState } from "react";
 import Data from "./Data.jsx";
-console.log(Data);
-
 import "./App.css";
 
 function App() {
-  const [name, setCount] = useState(Data);
-  setCount;
+  const [items, setItems] = useState(Data);
+  const [showText, setShowText] = useState({});
+
+  const handleToggle = (id) => {
+    // Toggle showing full text for a specific item
+    setShowText((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
+
+  const handleRemove = (id) => {
+    // Remove an item based on its ID
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+const refresh=()=>{
+  setItems(Data)
+}
   return (
-    <>
-      <div className="birthday-section">
-        <div className="birthdayAlign">
-          <h1>{name.length} person of birthday </h1>
-          {name.map(({ id, img, name, text }) => {
-            return (
-              <>
-              <article>
-<img src={img} alt={name} key={id} className="img" />
-              </article>
-              <div className="text">
-                 <h3>{name}</h3>
-                <p>{text}</p>
-              </div>
-              </>
-            );
-          })}
-        </div>
+    <div className="tours">
+      <div className="refresh">
+        <button className="btn" onClick={refresh}>refresh</button>
       </div>
-    </>
+      <div className="toursAlign">
+        {items.map(({ id, img, name, text, btn, read, price }) => (
+          <div className="section" key={id}>
+            <div className="money">
+              <span>{price}</span>
+            </div>
+            <article className="center">
+              <img src={img} className="img" alt={name} />
+              <h3>{name}</h3>
+              <p>{showText[id] ? text : text.substring(0, 100) + '...'}</p>
+              <span onClick={() => handleToggle(id)} className="read">
+                {read}
+              </span>
+              {showText[id] && <p className="show">{text}</p>}
+            </article>
+            <article>
+              <button className="btns" onClick={() => handleRemove(id)}>
+                {btn}
+              </button>
+            </article>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,36 +1,51 @@
 const Data = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/8859406/pexels-photo-8859406.jpeg?auto=compress&cs=tinysrgb&w=400",
-    name: "John",
-    text: "That is coder",
+    price:'$22,465',
+    img: "https://images.pexels.com/photos/2161449/pexels-photo-2161449.jpeg?auto=compress&cs=tinysrgb&w=400",
+    name: "best of paris in 7days tour",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi amet vero corrupti delectus ratione culpa, neque cum assumenda perspiciatis dicta sit sed porro exercitationem, alias magni, ut hic dolor!",
+    read :'read more',
+    btn:'not interest'
   },
 
   {
     id: 2,
-    img: "https://images.pexels.com/photos/9627808/pexels-photo-9627808.jpeg?auto=compress&cs=tinysrgb&w=400",
-    name: "Petter",
-    text: "That is developer",
+    price:'$73,465',
+    img: "https://images.pexels.com/photos/2161467/pexels-photo-2161467.jpeg?auto=compress&cs=tinysrgb&w=400",
+    name: "best of ireland in 14days tour",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi amet vero corrupti delectus ratione culpa, neque cum assumenda perspiciatis dicta sit sed porro exercitationem, alias magni, ut hic dolor!",
+    read :'read more',
+     btn:'not interest'
   },
 
   {
     id: 3,
-    img: "https://images.pexels.com/photos/9628358/pexels-photo-9628358.jpeg?auto=compress&cs=tinysrgb&w=400",
-    name: "Thomas",
-    text: "That is student",
+    price:'$7,465',
+    img: "https://images.pexels.com/photos/2108831/pexels-photo-2108831.jpeg?auto=compress&cs=tinysrgb&w=400",
+    name: "best of salzburg $ vienna in 8 days tour",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi amet vero corrupti delectus ratione culpa, neque cum assumenda perspiciatis dicta sit sed porro exercitationem, alias magni, ut hic dolor!",
+    read :'read more',
+     btn:'not interest'
   },
 
   {
     id: 4,
-    img: "https://images.pexels.com/photos/7180762/pexels-photo-7180762.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name: "Danilyuk",
-    text: "That is teacher",
+    price:'$33,465',
+    img: "https://images.pexels.com/photos/158398/niagara-falls-waterfall-horseshoe-158398.jpeg?auto=compress&cs=tinysrgb&w=400",
+    name: "best of romani in 7days tour",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi amet vero corrupti delectus ratione culpa, neque cum assumenda perspiciatis dicta sit sed porro exercitationem, alias magni, ut hic dolor!",
+    read :'read more',
+    btn:'not interest'
   },
   {
     id: 5,
-    img: "https://images.pexels.com/photos/8101624/pexels-photo-8101624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    name: "Sam",
-    text: "That is student",
+    price:'$23,465',
+    img: "https://images.pexels.com/photos/2147993/pexels-photo-2147993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "best or poland in 10days tour",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi amet vero corrupti delectus ratione culpa, neque cum assumenda perspiciatis dicta sit sed porro exercitationem, alias magni, ut hic dolor!",
+    read :'read more',
+     btn:'not interest'
   },
 ];
 export default Data;
